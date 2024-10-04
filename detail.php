e6381339657db2ac1a68e94f50813a7a5a303eb3
hfewcqocnq <141748219+hfewcqocnq@users.noreply.github.com>
2024-10-05 01:39:42 +0900

    Update detail.php

--- detail.php
+++ detail.php
@@ -24,25 +24,27 @@ if($status==false) {
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>NTNI 詳細</title>
-  <link rel="icon" type="img/site_logo.png" href="img/site_logo.png">
+  <title>詳細画面</title>
   <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
-  <div class="logo_senter">
-    <a href="top.php"><img class="logo" src="img/rogo.png"></a>
-  </div>
+  <img class="logo" src="img/rogo.png">
   <div class="container">
     <h1><?=$row["idea_title"]?></h1>
-    <a><?php 
-     $str1 = $row["idea_naiyou"] ;
-     $substr1 = mb_substr($str1, 0 ,20);
-      echo (''.$substr1.'<br><br>…続きは申請することで閲覧可能です<br><br>'); ?>
-      <!-- 文字列途中まで表示 --></a><br>
+    <a>
+      <?php if ($_SESSION["user_id"] === $row["user_id"]){
+        echo $row["idea_naiyou"];
+        }else {$str1 = $row["idea_naiyou"] ;
+        $substr1 = mb_substr($str1, 0 ,20);
+        echo (''.$substr1.'<br><br>…続きは申請することで閲覧可能です<br><br>');
+      } ?>
+    </a>
     <div class="detail_btn">
+    <?php if ($_SESSION["user_id"] !== $row["user_id"]): ?>
       <a href="request.php?idea_id=<?=h($row["idea_id"])?>">申請</a>
+    <?php endif; ?>
     　<a href="top.php">戻る</a>
-    </div>
+  </div>
   </div>
   <footer>
   <?php include("menu.php"); ?>
